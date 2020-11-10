f47ecdb9247e36eb2d7af445c2c256ed90e68082
Trần Đình Thiện <5782349-ThienUIT@users.noreply.gitlab.com>
2020-11-10 20:26:36 +0700

    upddate avatar

--- src/shared/middleware/auth.ts
+++ src/shared/middleware/auth.ts
@@ -3,13 +3,12 @@ import { UserRequest } from "@/config/request/user.requestt";
 
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("./async");
-const ErrorResponse = require("@/shared/utils/errorResponse");
+const { validation } = require("@/shared/utils/responseApi");
 const User = require("@/models/User");
 // protect route from away guest
 exports.protect = asyncHandler(
   async (req: UserRequest, res: Response, next: NextFunction) => {
     let token;
-
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
@@ -17,9 +16,11 @@ exports.protect = asyncHandler(
       token = req.headers.authorization.split(" ")[1];
     }
     if (!token) {
-      return next(
-        new ErrorResponse("Not authorized to access this route", 401)
-      );
+      return res
+        .status(401)
+        .json(
+          validation("Not authorized to access this route", res.statusCode)
+        );
     }
 
     try {
@@ -27,12 +28,14 @@ exports.protect = asyncHandler(
       const decoded = jwt.verify(token, process.env.TEXT_SECRET);
       // prototype.virtual subscribers count is
       req.user = await User.findById(decoded.id).populate("subscribers");
-      console.log("req.user", req.user._id);
+      console.log("middleWare::req.user::", req.user._id);
       next();
     } catch (err) {
-      return next(
-        new ErrorResponse("Not authorized to access this route", 401)
-      );
+      return res
+        .status(401)
+        .json(
+          validation("Not authorized to access this route", res.statusCode)
+        );
     }
   }
 );
@@ -40,12 +43,14 @@ exports.protect = asyncHandler(
 exports.authorize = (...roles: string[]) => {
   return (req: UserRequest, res: Response, next: NextFunction) => {
     if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorResponse(
-          `Your role ${req.user.role} is not authorized to access this route`,
-          403
-        )
-      );
+      return res
+        .status(403)
+        .json(
+          validation(
+            `Your role ${req.user.role} is not authorized to access this route`,
+            403
+          )
+        );
     }
     next();
   };
